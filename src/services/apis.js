f95056522b64d6b01332cd17aac3a231e7960093
Shivshantp21 <90241895+Shivshantp21@users.noreply.github.com>
2024-12-30 19:54:28 +0530

    Update apis.js

--- src/services/apis.js
+++ src/services/apis.js
@@ -1,5 +1,4 @@
-const BASE_URL = process.env.REACT_APP_BASE_URL
-
+const BASE_URL = "https://studynotion-backend-ftzm.onrender.com/api/v1"
 // AUTH ENDPOINTS
 export const endpoints = {
   SENDOTP_API: BASE_URL + "/auth/sendotp",
